26026bc329f92d4353c76205d199e3dc9b95032f
Hailey McLaughlin <116248501+HMcLaughlin16@users.noreply.github.com>
2023-05-28 10:49:48 -0400

    rock_paper_scissors.js

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -3,43 +3,49 @@ const getComputerChoice = [/"Paper"/i, /"Scissors"/i, /"Rock"/i];
     console.log(randGetComputerChoice, getComputerChoice[randGetComputerChoice]);
 
 function round(playerSelection, computerSelection) {
-    if (choice1 === "Paper") {
-        if (choice2 === "Rock") {
+    const choice1 = [/"Paper"/i || /"Scissors"/i || /"Rock"/i];
+    const choice2 = [/"Paper"/i || /"Scissors"/i || /"Rock"/i];
+    if (choice1 === /"Paper"/i) {
+        if (choice2 === /"Rock"/i) {
             return "Paper wins!";
         } else {
-            if (choice2 === "Scissors") {
+            if (choice2 === /"Scissors"/i) {
                 return "Scissors win!";
             } else {
-                if (choice2 === "Paper") {
+                if (choice2 === /"Paper"/i) {
                 return "It's a tie!";
                 }
             }
         }
     }
-    if (choice1 === "Rock") {
-        if (choice2 === "Paper") {
+    if (choice1 === /"Rock"/i) {
+        if (choice2 === /"Paper"/i) {
             return "Paper wins!";
         } else {
-            if (choice2 === "Scissors") {
+            if (choice2 === /"Scissors"/i) {
                 return "Rock wins!";
             } else {
-                if (choice2 === "Rock") {
+                if (choice2 === /"Rock"/i) {
                     return "It's a tie!";
                 }
             }
         }
     }
-    if (choice1 === "Scissors") {
-        if (choice2 === "Rock") {
+    if (choice1 === /"Scissors"/i) {
+        if (choice2 === /"Rock"/i) {
             return "Rock wins!";
         } else {
-            if (choice2 === "Paper") {
+            if (choice2 === /"Paper"/i) {
                 return "Scissors win!";
             } else {
-                if (choice2 === "Scissors") {
+                if (choice2 === /"Scissors"/i) {
                     return "It's a tie!";
                 }
             }
         }
     }
 }
+
+const playerSelection = /"Rock"/i;
+const computerSelection = randGetComputerChoice;
+console.log(round(playerSelection, computerSelection));
